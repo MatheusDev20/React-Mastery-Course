acd96bab6340757ac2d5eb9359d011838eea4893
Matheus de Paula <matheusdev20@gmail.com>
2020-01-05 17:54:58 -0300

    Adding Header and Links

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import {Route, Switch} from 'react-router-dom' // Switch impede que duas paginas
 
 import HomePage from './Pages/homepage/Homepage';
 import ShopPage from '../src/Pages/shop/ShopPage'
-
+import Header from '../src/Components/Header/Header'
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
       <Route exact path='/'component={HomePage}/>
       <Route path ="/shop" component={ShopPage}/>
